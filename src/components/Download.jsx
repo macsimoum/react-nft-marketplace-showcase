c3461354a2504c38e2ede46537a9274c08216890
macsimoum <a.r.hassannezhad@gmail.com>
2023-03-11 15:37:07 +0330

    v0

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -1,19 +1,32 @@
-import React from 'react';
+import React from "react";
 
-import styles from '../styles/Global';
-import assets from '../assets';
+import styles from "../styles/Global";
+import assets from "../assets";
 
 const Download = () => {
   return (
     <div className={`${styles.section} ${styles.bgWhite}`}>
       <div className={`${styles.subSection} flex-col text-center`}>
         <div>
-          <h1 className={`${styles.h1Text} ${styles.blackText}`}>Download the Source Code</h1>
-          <p className={`${styles.pText} ${styles.blackText}`}>Get the full source code on GitHub</p>
+          <h1 className={`${styles.h1Text} ${styles.blackText}`}>
+            Download the Source Code
+          </h1>
+          <p className={`${styles.pText} ${styles.blackText}`}>
+            Get the full source code on GitHub
+          </p>
         </div>
-        <button className={styles.btnPrimary}>Source Code</button>
+        <button
+          className={styles.btnPrimary}
+          onClick={() =>
+            window.open(
+              "https://github.com/macsimoum/react-native-nft-marketplace"
+            )
+          }
+        >
+          Source Code
+        </button>
         <div className={styles.flexCenter}>
-          <img 
+          <img
             src={assets.scene}
             alt="download_png"
             className={styles.fullImg}
@@ -21,7 +34,7 @@ const Download = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Download
+export default Download;
